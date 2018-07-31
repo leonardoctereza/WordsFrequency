e007d6b8d5422324f5a74eec3a846d723fb60ce7
Leonardo Carvalho Tereza <leonardoctereza@gmail.com>
2018-07-31 18:30:26 -0300

    Update wordsFrequency.py

--- wordsFrequency.py
+++ wordsFrequency.py
@@ -1,49 +1,93 @@
-#-*- coding: utf-8 -*-
-
+# -*- coding: utf-8 -*-
 """
-Created on Mon Jul 30 16:44:53 2018
+Created on Tue Jul 31 15:55:13 2018
 
-@author: leonardo
+@author: leonardot
 """
-from wordcloud import WordCloud
-import matplotlib.pyplot as plt
-import nltk
+
 import csv
+import nltk
+import string
+import unicodedata
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud
 
-#read stop words
-stopwordsBr = open('stopwords.txt', 'r',encoding='utf-8').read()
-#read the txt file
-word_list = open('exemplo.txt', 'r',encoding='utf-8').read().split(' ')
-#put all words in lowercase
-word_list = map(str.lower ,word_list)
-#remove stopwords and other stuffs
-filtered_words = [word.replace('.','').replace(',', '').replace('\n', '') for word in word_list 
-                  if word not in (stopwordsBr)]
+def remove_diacritic(input):
+    '''
+    Accept a unicode string, and return a normal string (bytes in Python 3)
+    without any diacritical marks.
+    '''
+    return unicodedata.normalize('NFKD', input).encode('ASCII', 'ignore')
 
 
-wordfreqdist = nltk.FreqDist(filtered_words)
-mostcommon = wordfreqdist.most_common(30)
+def clean_csv(file):
+    
+    csv_words = []
+    with open(file, 'r',encoding='utf-8') as csvfile:
+        spamreader = csv.reader(csvfile)
+        for row in spamreader:
+            str1 = ''
+            row_splited = nltk.word_tokenize(str1.join(row))
+            for word in row_splited:
+                wordD = str(word).lower().strip(string.punctuation)
+                wordD = remove_diacritic(wordD).decode()
+                csv_words.append(wordD)
+            
+    return csv_words
+    
 
-plt.figure(figsize=(15,10))
-plt.yticks(range(len(mostcommon)), [val[0] for val in mostcommon])
-plt.barh(range(len(mostcommon)),[val[1] for val in mostcommon], align='center')
-plt.savefig('mostcommonWords.png')
-plt.show()
+def plot_commonWords(mostcommon,filepath = 'mostcommonWords.png'):
+    
+    plt.figure(figsize=(15,10))
+    plt.yticks(range(len(mostcommon)), [val[0] for val in mostcommon])
+    plt.barh(range(len(mostcommon)),[val[1] for val in mostcommon], align='center')
+    plt.savefig(filepath)
+    plt.show()
+    
+    
+def save_words_frequency_csv(wordfreqlist, filepath= 'WordsFrequency.csv'):
+    with open(filepath, 'w') as csvfile:
+        filewriter = csv.writer(csvfile, delimiter='|')
+        filewriter.writerow(['Palavra', 'Quantidade'])
+        for key in wordfreqlist:
+            filewriter.writerow([key,wordfreqlist[key]])
+            
 
-with open('WordsFrequency.csv', 'w') as csvfile:
-    filewriter = csv.writer(csvfile, delimiter='|')
-    filewriter.writerow(['Palavra', 'Quantidade'])
-    for key in wordfreqdist:
-        filewriter.writerow([key,wordfreqdist[key]])
+def generate_wordcloud(filtered_words,maxwords=30):
+    wordcloud = WordCloud(max_words=maxwords    
+                         ).generate(' '.join(map(str, filtered_words)))
+    plt.clf()
+    plt.imshow(wordcloud, interpolation='bilinear')
+    plt.axis('off')
+    plt.savefig('imageCloud.png',dpi = 100)
+    plt.show()
+    
 
-#wordcloud 
-#wordcloud = WordCloud(max_words=100    
-#                         ).generate('\n'.join(map(str, filtered_words)))
-#
-#
-#
-#plt.clf()
-#plt.imshow(wordcloud, interpolation='bilinear')
-#plt.axis('off')
-#plt.savefig('imageCloud.png',dpi = 100)
-#plt.show()
+def clean_stopwords():
+    stopwords = []
+    file = open('stopwords.txt', 'r',encoding='utf-8')
+    for word in file:
+        stopwords.append(remove_diacritic(word).decode().lower().strip(string.punctuation).replace('\n', '').replace(' ', ''))
+    return stopwords
+    
+
+def deal_csv(file):
+    
+    stopwordsBr = clean_stopwords()
+    word_list = clean_csv(file)
+    
+    filtered_words = [word.replace('.','').replace(',', '').replace('\n', '') for word in word_list 
+                  if( word not in stopwordsBr 
+                      and len(word) > 3) 
+                      and not word.isdigit()]
+    
+    wordfreqlist = nltk.FreqDist(filtered_words)
+    mostcommon = wordfreqlist.most_common(30)
+    plot_commonWords(mostcommon)
+    save_words_frequency_csv(wordfreqlist)
+    generate_wordcloud(filtered_words)
+    
+
+deal_csv('exemplo.csv')
+
+ 
